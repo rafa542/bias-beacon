b22bddb1c8f0acf42e029d8f84949992f1fe53ef
Yeshwanth Somu <111399301+ysomu88@users.noreply.github.com>
2024-02-05 19:56:34 -0500

    Add files via upload

---
+++ notebooks/MBIB-main/baseline/README.md
@@ -0,0 +1,108 @@
+# Baseline training & evaluation
+Implementation of the experiments performed in the paper. See baseline performance on various tested models below.
+Baseline performances are the best results of the five transfomer models tested:
+
+
+| Task | Model | Micro F1 | Macro F1 |
+| --- | --- | --- | --- |
+| cognitive-bias | ConvBERT/ConvBERT | 0.7126 | 0.7664 |
+| fake-news | Bart/RoBERTa-T | 	0.6811 | 0.7533 |
+| gender-bias | RoBERTa-T/ELECTRA | 0.8334 | 0.8211 |
+| hate-speech | RoBERTA-T/Bart | 0.8897 | 0.7310 |
+| linguistic-bias | ConvBERT/Bart | 0.7044 | 0.4995 |
+| political-bias | ConvBERT/ConvBERT | 0.7041 | 0.7110 |
+| racial-bias | ConvBERT/ELECTRA | 0.8772 | 	0.6170 |
+| text-level-bias | ConvBERT/ConvBERT | 0.7697 | 	0.7532 |
+
+## Reproduce results
+In order to reproduce the baseline results run the training script:
+```
+python baseline/run_baseline.py
+```
+
+## Files descriptions
+* `run_baseline.py`
+  * script where specifics of the training are defined, see example below
+   ```python
+    wrapper = TrainerWrapper(5, 'cognitive-bias', "bart", gpu=0,batch_size=64, model_length=78)
+    result = wrapper.run()
+   ```
+* `trainer/model_specifications.py`
+    - This file is used by `TrainerWrapper.py` for specification of the model, which is accessed through huggingface.
+    - the following models were evaluated, models were chosen because they showed best performances on the BABE dataset:
+      * [ConvBert](https://huggingface.co/YituTech/conv-bert-base)
+      * [Bart](https://huggingface.co/facebook/bart-base)
+      * [TwitterRoBERTa](https://huggingface.co/cardiffnlp/twitter-roberta-base)
+      * [GPT2](https://huggingface.co/gpt2)
+      * [Electra](https://huggingface.co/google/electra-base-discriminator)
+* `trainer/TrainerWrapper.py`
+  * This is a wrapper class that wraps tokenization, configuration and training of the model into k-fold cross-validation.
+* `trainer/ModelTrainer.py`
+  * Main training class using accelerator for improved training and [Weights & Biases](https://wandb.ai/home) for logging. If you want to use the weights & biases, put your wandb API key to the `config.py`
+  
+## Result analysis
+All baseline evaluation results: 
+|	Bias Type	|	Rank	|	Model	|	Macro-Score	|	Rank	|	Model	|	Micro-Score	|
+|	---	|	---	|	---	|	---	|	---	|	---	|	---	|
+|	Linguistic Bias	|	1	|	Bart	|	0.7664	|	1	|	ConvBert	|	0.7126	|
+|		|	2	|	RoBERTa-Twitter	|	0.7479	|	2	|	ELECTRA	|	0.7122	|
+|		|	3	|	GPT-2	|	0.7459	|	3	|	Bart	|	0.7106	|
+|		|	4	|	ConvBERT &	|	0.7283	|	4	|	Roberta-Twitter	|	0.7102	|
+|		|	5	|	ELECTRA	|	0.7136	|	5	|	GPT2	|	0.7011	|
+|		|		|		|		|		|		|		|
+|	Cognitive Bias	|	1	|	ConvBERT	|	0.4995	|	1	|	ConvBert	|	0.7044	|
+|		|	2	|	RoBERTa-Twitter	|	0.4986	|	2	|	Bart	|	0.7042	|
+|		|	3	|	GPT-2	|	0.4968	|	3	|	Roberta-Twitter	|	0.7006	|
+|		|	4	|	ELECTRA	|	0.4949	|	4	|	GPT2	|	0.6976	|
+|		|	5	|	Bart	|	0.4881	|	5	|	ELECTRA	|	0.6777	|
+|		|		|		|		|		|		|		|
+|	Text-Level Context Bias	|	1	|	ConvBERT	|	0.7532	|	1	|	ConvBert	|	0.7697	|
+|		|	2	|	Bart	|	0.7477	|	2	|	Roberta-Twitter	|	0.7689	|
+|		|	3	|	RoBERTa-Twitter	|	0.7382	|	3	|	Bart	|	0.7622	|
+|		|	4	|	ELECTRA	|	0.7347	|	4	|	ELECTRA	|	0.7532	|
+|		|	5	|	GPT-2	|	0.7075	|	5	|	GPT2	|	0.7447	|
+|		|		|		|		|		|		|		|
+|	Hate Speech	|	1	|	Bart	|	0.731	|	1	|	Roberta-Twitter	|	0.8897	|
+|		|	2	|	ConvBERT	|	0.7248	|	2	|	GPT2	|	0.8824	|
+|		|	3	|	RoBERTa-Twitter	|	0.7229	|	3	|	ELECTRA	|	0.8821	|
+|		|	4	|	GPT-2	|	0.7198	|	4	|	ConvBert	|	0.8805	|
+|		|	5	|	ELECTRA	|	0.7184	|	5	|	Bart	|	0.8797	|
+|		|		|		|		|		|		|		|
+|	Gender Bias	|	1	|	ELECTRA	|	0.8211	|	1	|	Roberta-Twitter	|	0.8334	|
+|		|	2	|	Bart	|	0.8168	|	2	|	Bart	|	0.8333	|
+|		|	3	|	ConvBERT	|	0.8119	|	3	|	ELECTRA	|	0.8305	|
+|		|	4	|	RoBERTa-Twitter	|	0.8116	|	4	|	ConvBert	|	0.8257	|
+|		|	5	|	GPT-2	|	0.7852	|	5	|	GPT2	|	0.8134	|
+|		|		|		|		|		|		|		|
+|	Racial Bias	|	1	|	ELECTRA	|	0.617	|	1	|	ConvBert	|	0.8772	|
+|		|	2	|	ConvBERT	|	0.6153	|	2	|	ELECTRA	|	0.8768	|
+|		|	3	|	Bart	|	0.6103	|	3	|	Roberta-Twitter	|	0.8728	|
+|		|	4	|	RoBERTa-Twitter	|	0.607	|	4	|	Bart	|	0.8693	|
+|		|	5	|	GPT-2	|	0.5961	|	5	|	GPT2	|	0.8508	|
+|		|		|		|		|		|		|		|
+|	Fake News	|	1	|	RoBERTa-Twitter	|	0.7533	|	1	|	Bart	|	0.6811	|
+|		|	2	|	ConvBERT	|	0.7382	|	2	|	ConvBert	|	0.6787	|
+|		|	3	|	Bart	|	0.7236	|	3	|	Roberta-Twitter	|	0.6721	|
+|		|	4	|	ELECTRA	|	0.7049	|	4	|	ELECTRA	|	0.6574	|
+|		|	5	|	GPT-2	|	0.6596	|	5	|	GPT2	|	0.6094	|
+|		|		|		|		|		|		|		|
+|	Political Bias	|	1	|	ConvBERT	|	0.711	|	1	|	ConvBert	|	0.7041	|
+|		|	2	|	RoBERTa-Twitter	|	0.7036	|	2	|	Roberta-Twitter	|	0.7021	|
+|		|	3	|	ELECTRA	|	0.6989	|	3	|	Bart	|	0.6997	|
+|		|	4	|	GPT-2	|	0.6909	|	4	|	GPT2	|	0.696	|
+|		|	5	|	Bart	|	0.6804	|	5	|	ELECTRA	|	0.6255	|
+
+This results into the following final MBIB scores:
+
+|	Model	|	Final Macro-Score	|	Final Micro-Score	|
+|	---	|	---	|	---	|
+|	Bart	|	0.6955375	|	0.7675125	|
+|	ConvBERT	|	0.697775	|	0.7691125	|
+|	ELECTRA	|	0.6879375	|	0.751925	|
+|	GPT-2	|	0.675225	|	0.749425	|
+|	RoBERTa-Twitter	|	0.6978875	|	0.768725	|
+
+
+
+The figure below depicts the performance of all models per dataset:
+![d](../figures/per_dataset_performance.png)
---
+++ notebooks/MBIB-main/baseline/trainer/BaselineTrainer.py
@@ -0,0 +1,193 @@
+import time
+
+import pandas as pd
+import torch
+import wandb
+from accelerate import Accelerator
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, SubsetRandomSampler
+from tqdm import trange
+from tqdm.auto import tqdm
+from transformers import get_scheduler
+from config import WANDB_API_KEY
+
+
+class BaselineTrainer:
+    def __init__(self, category, model_name):
+        self.max_epochs = 10
+        self.category = category
+        self.model_name = model_name
+        self.gpu_available = torch.cuda.is_available()
+
+    def fit(self, model, optimizer, train_dataloader, dev_dataloader, device, accelerator, lr_scheduler):
+        """Method for Training loop with Early Stopping based on the DevSet"""
+        num_training_steps = self.max_epochs * len(train_dataloader)
+        progress_bar = tqdm(range(num_training_steps))
+
+        # EARLY STOPPING CRITERIA
+        # Source of the Early Stopping: https://pythonguides.com/pytorch-early-stopping/
+        last_loss = 100
+        patience = 1
+        trigger = 0
+
+        for epoch in trange(self.max_epochs, desc='Epoch'):
+            print(f'Started Training Epoch {epoch}')
+            # Training
+            model.train()
+            for step, batch in enumerate(train_dataloader, start=1):
+                with accelerator.accumulate(model):
+                    batch = {k: v.to(device) for k, v in batch.items()}
+                    if self.model_name == 'convbert' or self.model_name == 'electra':
+                        outputs = model(input_ids=batch['input_ids'], token_type_ids=batch['token_type_ids'],
+                                        attention_mask=batch['attention_mask'], labels=batch['labels'])
+                    else:
+                        outputs = model(
+                            input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], labels=batch['labels'])
+                    loss = outputs.loss
+                    accelerator.backward(loss)
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    lr_scheduler.step()
+                    progress_bar.update(1)
+                    wandb.log({"batch": step, "time": time.time()})
+
+            # Evaluation on DevSet
+            model.eval()
+            loss_lst, dev_predictions, dev_actuals = [], [], []
+            for batch in dev_dataloader:
+                batch = {k: v.to(device) for k, v in batch.items()}
+                with torch.no_grad():
+                    if self.model_name == 'convbert' or self.model_name == 'electra':
+                        outputs = model(input_ids=batch['input_ids'], token_type_ids=batch['token_type_ids'],
+                                        attention_mask=batch['attention_mask'], labels=batch['labels'])
+                    else:
+                        outputs = model(
+                            input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], labels=batch['labels'])
+                logits = outputs.logits
+                loss = outputs.loss
+                loss_lst.append(loss)
+                dev_actuals.extend(batch['labels'])
+                dev_predictions.extend(torch.argmax(logits, dim=-1))
+
+            current_loss = sum(loss_lst) / len(loss_lst)
+            wandb.log({"loss": current_loss, "epoch": epoch})
+            dev_predictions = torch.stack(dev_predictions).cpu()
+            dev_actuals = torch.stack(dev_actuals).cpu()
+            dev_report = classification_report(dev_actuals, dev_predictions, target_names=['non-biased', 'biased'],
+                                               output_dict=True)
+            wandb.log(
+                {"DEV f-1 score": dev_report['weighted avg']['f1-score'], "epoch": epoch})
+            print('The current dev loss:', current_loss)
+            if current_loss >= last_loss:
+                trigger += 1
+                print('trigger times:', trigger)
+
+                if trigger >= patience:
+                    print('Early stopping!\n Starting evaluation on test set.')
+                    break
+
+            else:
+                print('trigger: 0')
+                trigger = 0
+            last_loss = current_loss
+        return model
+
+    def evaluate(self, model, test_dataloader, device, fold):
+        """Evaluation model on the Test set"""
+        num_test_steps = len(test_dataloader)
+        progress_bar = tqdm(range(num_test_steps))
+
+        print(f'Start Evaluation')
+        predictions, actuals, datasets = [], [], []
+        for batch in test_dataloader:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            with torch.no_grad():
+                if self.model_name == 'convbert' or self.model_name == 'electra':
+                    outputs = model(input_ids=batch['input_ids'], token_type_ids=batch['token_type_ids'],
+                                    attention_mask=batch['attention_mask'], labels=batch['labels'])
+                else:
+                    outputs = model(
+                        input_ids=batch['input_ids'], attention_mask=batch['attention_mask'], labels=batch['labels'])
+            logits = outputs.logits
+            actuals.extend(batch['labels'])
+            predictions.extend(torch.argmax(logits, dim=-1))
+            datasets.extend(batch['dataset_id'])
+            progress_bar.update(1)
+
+        predictions = torch.stack(predictions).cpu()
+        actuals = torch.stack(actuals).cpu()
+        datasets = torch.stack(datasets).cpu()
+        report = classification_report(actuals, predictions, target_names=[
+                                       'non-biased', 'biased'], output_dict=True)
+        f1_score = report['weighted avg']['f1-score']
+        wandb.log({"TEST f-1 score": f1_score, "fold": fold})
+        df_report = pd.DataFrame(report)
+        df_report.to_csv(
+            f'./Results_new/{self.model_name}-{self.category}-fold-{fold}-report.csv')
+        df_predictions = pd.DataFrame(
+            data={'predictions': predictions, 'actuals': actuals, 'dataset_id': datasets})
+        # Save the predictions for later analysis
+        df_predictions.to_csv(
+            f'./Results_new/{self.model_name}-{self.category}-fold-{fold}-predictions.csv')
+        return f1_score
+
+    def main(self, fold, train_ids, val_ids, data, model, learning_rate, batch_size, gpu_no):
+        """Main Method calling the training and evaluation, starting wandb, setting the GPU, and initializes e.g. Optimizer and Accelerator"""
+        print(f'Training Initialized for fold {fold}')
+        # Initialize Weights & Biases
+        wandb.login(key =WANDB_API_KEY, relogin = True)
+        wandb.init(project=str(self.category) + str(self.model_name), reinit=True)
+        wandb.config = {
+            "learning_rate": learning_rate,
+            "epochs": 20,
+            "batch_size": batch_size,
+        }
+        wandb.run.name = "Fold-" + str(fold)
+
+        # Set the GPU
+        device =  torch.device("cpu")
+
+        # Create DEV and TEST Set from the K-folds Test Set
+        # DEV Set used for early stopping criteria, the test set only for final evaluation
+        dev_ids, test_ids = train_test_split(
+            val_ids, test_size=0.75, train_size=0.25, random_state=42, shuffle=True)
+
+        train_sampler = SubsetRandomSampler(train_ids)
+        dev_sampler = SubsetRandomSampler(dev_ids)
+        test_sampler = SubsetRandomSampler(test_ids)
+
+        train_dataloader = DataLoader(
+            data, batch_size=batch_size, sampler=train_sampler)
+        dev_dataloader = DataLoader(
+            data, batch_size=batch_size, sampler=dev_sampler)
+        test_dataloader = DataLoader(
+            data, batch_size=batch_size, sampler=test_sampler)
+
+        # Push model to GPU
+        model.to(device)
+        optimizer = torch.optim.AdamW(
+            model.parameters(), lr=learning_rate)  # Initialize Optimizer
+        # Enable gradient checkpointing to save memory
+        model.gradient_checkpointing_enable()
+        lr_scheduler = get_scheduler(
+            "cosine",
+            optimizer=optimizer,
+            num_warmup_steps=0,
+            num_training_steps=10 * len(train_dataloader)
+        )
+        # Start Accelerator See https://huggingface.co/docs/transformers/v4.20.1/en/perf_train_gpu_one
+        accelerator = Accelerator(
+            device_placement=False, cpu=True, gradient_accumulation_steps=4)
+        model, optimizer,_,lr_scheduler = accelerator.prepare(
+            model, optimizer, train_dataloader, lr_scheduler)
+
+        # Model Training with Dev Evaluation for Early Stopping
+        model = self.fit(model, optimizer, train_dataloader,
+                         dev_dataloader, device, accelerator, lr_scheduler)
+
+        # Evaluation on TestSet
+        score = self.evaluate(model, test_dataloader, device, fold)
+
+        wandb.finish()
+        return score
