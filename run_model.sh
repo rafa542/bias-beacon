caec235d147e2e2b843e57befacb606a09024ec6
Georgiy Sekretaryuk <37413530+sekretaryuk@users.noreply.github.com>
2024-02-06 19:32:44 -0500

    Shipping updates

--- run_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if an argument is provided
-if [ $# -eq 0 ]; then
-    echo "No word provided. Usage: ./run_model.sh [word]"
-    exit 1
-fi
-
-python3 model/dummy_model.py "$1"
